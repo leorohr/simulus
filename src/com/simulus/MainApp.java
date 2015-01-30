5a77dab9a4110756283d0be09cc2c67e944b17a8
leorohr <rohrleo@googlemail.com>
2015-01-30 15:32:02 +0000

    Initialise MainApp

--- src/com/simulus/MainApp.java
+++ src/com/simulus/MainApp.java
@@ -1,15 +1,73 @@
 package com.simulus;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MainApp extends Application {
 
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+	
+	
 	@Override
 	public void start(Stage primaryStage) {
+	
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("Simulus");
+		
+		initRootLayout();
+		showMainView();
+	
+	}
+	
+	/**
+	 * Initialise the root layout
+	 */
+	private void initRootLayout() {
+		
+		try {
+			
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 
+	/**
+	 * Shows the cashflow overview interface
+	 */
+	private void showMainView() {
+		
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/MainView.fxml"));
+			AnchorPane overview = (AnchorPane) loader.load();
+		
+			//Set overview as center widget of the pane
+			rootLayout.setCenter(overview);
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
