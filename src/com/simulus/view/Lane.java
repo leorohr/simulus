0c4340b463e831acf2a57119e9911d8a505f627a
ebrahimmalek <ebrahimmalek@hotmail.co.uk>
2015-02-20 12:42:47 +0000

    Stuff

---
+++ src/com/simulus/view/Lane.java
@@ -0,0 +1,23 @@
+package com.simulus.view;
+
+
+import javafx.scene.paint.Color;
+
+import com.simulus.util.enums.Direction;
+
+public class Lane extends Tile{
+	
+	private Direction dir;
+
+	public Lane(double posX, double posY, double width, double height,
+			int gridPosX, int gridPosY, Direction dir) {
+		super(posX, posY, width, height, gridPosX, gridPosY);
+		this.dir = dir;
+		getFrame().setFill(Color.BLACK);
+	}
+	
+	
+	public Direction getDirection() {
+		return this.dir;
+	}
+}
