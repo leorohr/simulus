f26a6db26e0bc1850c92b8adf61ed1831fe2195a
ebrahimmalek <ebrahimmalek@hotmail.co.uk>
2015-03-03 19:01:29 +0000

    Paths that hold information on the start tile, end tile, duration , distance etc.

---
+++ src/com/simulus/view/CustomPath.java
@@ -0,0 +1,52 @@
+package com.simulus.view;
+
+import java.util.Collection;
+
+import com.simulus.util.enums.Direction;
+
+import javafx.animation.RotateTransition;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.PathElement;
+
+public class CustomPath extends Path{
+	
+	private Tile startTile;
+	private Tile endTile;
+	private Direction endDir;
+	private double pathDistance;
+	private String turn;
+	
+	public CustomPath(String turn, double pathDistance, Tile st, Tile et, Direction ed, PathElement... elements){
+		super(elements);
+		startTile = st;
+		endTile = et;
+		endDir = ed;
+		this.pathDistance = pathDistance;
+		this.turn = turn;
+	}
+		
+	public void setStartTile(Tile t){
+		startTile = t;
+	}
+	public void setEndTile(Tile t){
+		endTile = t;
+	}
+	public Tile getStartTile(){
+		return startTile;
+	}
+	public Tile getEndTile(){
+		return endTile;
+	}
+	public void setEndDir(Direction d){
+		endDir = d;
+	}
+	public Direction getEndDirection(){
+		return endDir;
+	}
+	public double getDistance(){
+		return pathDistance;
+	}
+	public String getTurn(){
+		return turn;
+	}
+}
