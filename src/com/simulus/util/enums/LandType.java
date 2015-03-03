0e09993e77fb5ea2209fbd615ec6b23e5bf2b0e0
Paul Barella <paul.barella@kcl.ac.uk>
2015-03-03 18:26:56 +0000

    Created a Land tile and 3 Classes that extend it to allow the user to draw nicer maps. city.png still needed

---
+++ src/com/simulus/util/enums/LandType.java
@@ -0,0 +1,31 @@
+/*
+ * @(#) LandTypes.java 
+ * 
+ * Copyright (c) 2015 Team Simulus at King's College London. All Rights Reserved. 
+ * 
+ * This software is the confidential and proprietary of Team Simulus 
+ * 7CCSMGPR Group Project(14~15 SEM2 1) at Kings' College London. 
+ * You shall not disclose such confidential information and shall use it only in accordance 
+ * with the agreement and regulation set by Simulus and the Department of Informatics at King's College London. 
+ * 
+ * The software is set to be reviewed by module coordinator of 7CCSMGPR Group Project(14~15 SEM2 1)
+ * 
+ */
+
+/* 
+ * VERSION HISTORY:
+ * 
+ * 1.00 03/03/2015 Initial Version  
+ */
+
+/**
+ * Enumerations for the types of non-road (Land) tiles
+ *@author Paul Barella
+ *@since 1.00
+ *@version 1.00 03/03/2015
+ */
+package com.simulus.util.enums;
+
+public enum LandType {
+	GRASS, DIRT, CITY, EMPTY
+}
