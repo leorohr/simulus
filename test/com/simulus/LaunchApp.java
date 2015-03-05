67451d844a42018357a5a8d87d29111ef86114ec
jerry <jerrylu.wang@gmail.com>
2015-03-05 14:49:36 +0000

    TestCase1 stable - scripts should execute with all tests pass

--- test/com/simulus/LaunchApp.java
+++ test/com/simulus/LaunchApp.java
@@ -1,36 +1,62 @@
-/*
- * @(#) Launcher.java 
- * 
- * Copyright (c) 2015 Team Simulus at King's College London. All Rights Reserved. 
- * 
- * This software is the confidential and proprietary of Team Simulus 
- * 7CCSMGPR Group Project(14~15 SEM2 1) at Kings' College London. 
- * You shall not disclose such confidential information and shall use it only in accordance 
- * with the agreement and regulation set by Simulus and the Department of Informatics at King's College London . 
- * 
- * The software is set to be reviewed by module coordinator of 7CCSMGPR Group Project(14~15 SEM2)
- * 
- */
-
-/* 
- * VERSION HISTORY:
- * 
- * 1.00 01/03/2015 Initial Version  
- */
-
 package com.simulus;
 
-import org.jemmy.fx.AppExecutor;
+import javafx.application.Application;
 
+public class LaunchApp {
 
-public class LaunchApp {     //Use JemmyFX test framework to launch the application
-		
-	
-	
-	public static void main (String[] args) {
-		
-		AppExecutor.execute(MainApp.class);
-	
+	private Thread appThread;
+	Thread returnedThread;
+	boolean threadFound = false;
+	boolean isAlive = false;
+	boolean isInterrupted = false;
+
+	public void launchApp(){
+
+		appThread = new Thread("JavaFX Init Thread") {
+			public void run() {
+			
+				Application.launch(MainApp.class, new String[0]);
+			}
+		};
+
+		appThread.setDaemon(true);
+		appThread.start();
 	}
+
 	
+	public Thread threadStatus(String threadName) {
+
+		for(Thread allThread : Thread.getAllStackTraces().keySet()){
+			if(allThread.getName().equals(threadName));
+			
+			threadFound = true;
+			returnedThread = allThread;
+
+			chceckThreadStatus();
+		}
+
+		return returnedThread;
+
+	}
+
+	
+	private void chceckThreadStatus( ) {
+		if(returnedThread.isAlive()){
+			isAlive = true;
+		}
+		if(returnedThread.isInterrupted()){
+			isInterrupted = true;
+		}
+	}
+
+	public Thread  getThreadByName(String threadName) {
+		Thread threadFound = null;
+
+		for (Thread t : Thread.getAllStackTraces().keySet()) {
+			if (t.getName().equals(threadName)) 
+				threadFound = t;
+		}
+		return threadFound;
+	}
+
 }
