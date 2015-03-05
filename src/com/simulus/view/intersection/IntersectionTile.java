329cd5a26601513c15d2c1cc4037f4965e44659c
Alan <alan.suleiman@kcl.ac.uk>
2015-03-05 21:26:34 +0000

    Editor loading map correctly again.  Now inlign previous to merge.

--- src/com/simulus/view/intersection/IntersectionTile.java
+++ src/com/simulus/view/intersection/IntersectionTile.java
@@ -2,6 +2,10 @@ package com.simulus.view.intersection;
 
 import java.util.ArrayList;
 
+import javafx.scene.paint.ImagePattern;
+
+import com.simulus.util.ResourceBuilder;
+import com.simulus.util.enums.Direction;
 import com.simulus.view.Tile;
 
 public class IntersectionTile extends Tile {
@@ -13,6 +17,7 @@ public class IntersectionTile extends Tile {
 			double height, int gridPosX, int gridPosY) {
 		
 		super(posX, posY, width, height, gridPosX, gridPosY);
+		frame.setFill(new ImagePattern((ResourceBuilder.getBoxjunctionTexture())));
 		
 		turningPaths = new ArrayList<>();
 	}
