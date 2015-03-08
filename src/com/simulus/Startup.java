4fea251c4262ff6c69abd3d6119152b28190f8b5
leorohr <rohrleo@googlemail.com>
2015-03-08 22:36:13 +0000

    minor changes ignore csv file

--- src/com/simulus/Startup.java
+++ src/com/simulus/Startup.java
@@ -27,8 +27,6 @@ public class Startup extends Application {
 		Button simBtn = new Button("Simulator");
 		simBtn.setPrefSize(100.0d, 50.0d);
 		simBtn.setOnAction((event) -> {
-			//Leo to confirm
-			//MainApp app = MainApp.getInstance();
 			MainApp app = new MainApp();
 			app.start(primaryStage);
 		});
